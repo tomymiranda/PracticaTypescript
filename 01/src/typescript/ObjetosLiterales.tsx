edbec9c3c6922282f6386d8924f1ccb9bdff353b
TomyMiranda <tomasmiranda98@hotmail.com>
2022-05-19 12:51:26 -0300

    se introduce interfaces

--- 01/src/typescript/ObjetosLiterales.tsx
+++ 01/src/typescript/ObjetosLiterales.tsx
@@ -1,9 +1,17 @@
 import React from 'react'
-import { json } from 'stream/consumers'
+interface Persona {
+    nombre: string,
+    edad: number,
+    direccion: Direccion,
+}
+/* una interfaz abajo de otra, primero la principal, solo sirve para validar tipos*/
+interface Direccion {
+  calle: string;
+  pais: string;
+}
 
-const ObjetosLiterales = () => {
-  
-  const persona = {
+const ObjetosLiterales = () => { 
+  const persona: Persona = {
     nombre: 'Juan',
     edad: 23,
     direccion: {
@@ -16,7 +24,7 @@ const ObjetosLiterales = () => {
       <div>
         <h3>Objeto literal</h3>
         <code>
-            {/*el replacer[donde va null] sirve  */}
+            {/*el replacer[donde va null] sirve ver q tipo de dato  quiero mostrar */}
           <pre>{JSON.stringify(persona,null,2)}</pre>
         </code>
       </div>
