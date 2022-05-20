3ea4a560ea683418cd9aa5a0a835f45ac2afde4d
Tomas Miranda <tomasmiranda98@hotmail.com>
2022-05-20 10:23:56 -0300

    se agregan hooks

--- 01/src/App.tsx
+++ 01/src/App.tsx
@@ -1,15 +1,21 @@
 import React from 'react'
 import TiposBasicos from '../src/typescript/TiposBasicos'
+import Contador from './components/Contador';
+import Funciones from './typescript/Funciones';
 import ObjetosLiterales from './typescript/ObjetosLiterales';
+
 
 const App = () => {
   return (
     <div className='mt-2'>
-      <h1>Titulo ahre</h1>
+      <h1>Titulo</h1>
       <hr></hr>
       {/*<TiposBasicos/>*/}
-      <ObjetosLiterales/>
+      {/*<ObjetosLiterales/>*/}
+      {/*<Funciones/>*/}
+      <Contador/>
     </div>
+      
   )
 }
 
